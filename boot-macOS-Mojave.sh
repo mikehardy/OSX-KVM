7fedbacb2a41b18919b4b5a618729302e6e543ba
Mike Hardy <github@mikehardy.net>
2019-02-21 15:34:27 -0500

    This works correctly and generates a local socket monitor

--- boot-macOS-Mojave.sh
+++ boot-macOS-Mojave.sh
@@ -13,9 +13,9 @@
 
 MY_OPTIONS="+pcid,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 
-qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
+qemu-system-x86_64 -enable-kvm -m 4096 -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
 	  -machine pc-q35-2.11 \
-	  -smp 4,cores=2 \
+	  -smp 8,cores=4,threads=2 \
 	  -usb -device usb-kbd -device usb-tablet \
 	  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
 	  -drive if=pflash,format=raw,readonly,file=OVMF_CODE.fd \
@@ -26,7 +26,5 @@ qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+i
 	  -drive id=Clover,if=none,snapshot=on,format=qcow2,file=./'Mojave/Clover.qcow2' \
 	  -device ide-drive,bus=ide.1,drive=MacHDD \
 	  -drive id=MacHDD,if=none,file=./mac_hdd.img,format=qcow2 \
-	  -device ide-drive,bus=ide.0,drive=MacDVD \
-	  -drive id=MacDVD,if=none,snapshot=on,media=cdrom,file=./'Mojave-10.14.3.iso' \
 	  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
-	  -monitor stdio
+	  -monitor unix:/home/mike/work/OSX-KVM/monitor,server,nowait
