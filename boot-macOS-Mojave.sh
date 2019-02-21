f0c043ed83ac93f7cba749f4dbe75c9ba6f46950
Mike Hardy <github@mikehardy.net>
2019-02-21 16:34:31 -0500

    Use absolute paths so we can start this from launcher

--- boot-macOS-Mojave.sh
+++ boot-macOS-Mojave.sh
@@ -18,13 +18,13 @@ qemu-system-x86_64 -enable-kvm -m 4096 -cpu Penryn,kvm=on,vendor=GenuineIntel,+i
 	  -smp 8,cores=4,threads=2 \
 	  -usb -device usb-kbd -device usb-tablet \
 	  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
-	  -drive if=pflash,format=raw,readonly,file=OVMF_CODE.fd \
-	  -drive if=pflash,format=raw,file=OVMF_VARS-1024x768.fd \
+	  -drive if=pflash,format=raw,readonly,file=/home/mike/work/OSX-KVM/OVMF_CODE.fd \
+	  -drive if=pflash,format=raw,file=/home/mike/work/OSX-KVM/OVMF_VARS-1024x768.fd \
 	  -smbios type=2 \
 	  -device ich9-intel-hda -device hda-duplex \
 	  -device ide-drive,bus=ide.2,drive=Clover \
-	  -drive id=Clover,if=none,snapshot=on,format=qcow2,file=./'Mojave/Clover.qcow2' \
+	  -drive id=Clover,if=none,snapshot=on,format=qcow2,file=/home/mike/work/OSX-KVM/Mojave/Clover.qcow2 \
 	  -device ide-drive,bus=ide.1,drive=MacHDD \
-	  -drive id=MacHDD,if=none,file=./mac_hdd.img,format=qcow2 \
+	  -drive id=MacHDD,if=none,file=/home/mike/work/OSX-KVM/mac_hdd.img,format=qcow2 \
 	  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
 	  -monitor unix:/home/mike/work/OSX-KVM/monitor,server,nowait
