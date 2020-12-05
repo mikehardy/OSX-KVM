19c6edc0aa4370de7325eca8e3fb38c9d491aa29
Mike Hardy <github@mikehardy.net>
2020-12-05 09:16:39 -0500

    use more RAM for OSX VM

--- OpenCore-Boot.sh
+++ OpenCore-Boot.sh
@@ -18,7 +18,7 @@ MY_OPTIONS="+vmx,+pcid,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 # This script works for Big Sur, Catalina, Mojave, and High Sierra. Tested with
 # macOS 10.15.6, macOS 10.14.6, and macOS 10.13.6
 
-ALLOCATED_RAM="8192" # MiB
+ALLOCATED_RAM="10240" # MiB
 CPU_SOCKETS="3"
 CPU_CORES="2"
 CPU_THREADS="2"
